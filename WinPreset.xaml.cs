077730cc8b3844a4372ce3123c4fd64287a414cb
Andrew Lochbaum <andrew.lochbaum@live.com>
2022-08-05 10:50:13 -0400

    Building new type from scratch, with button

--- WinPreset.xaml.cs
+++ WinPreset.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CreateMultFiles
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WinPreset.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WinPreset : Window
     {
-        public MainWindow()
+        public WinPreset()
         {
             InitializeComponent();
-        }
-
-        private void New_Preset_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
